3e2d769e8b1b4866d0181d7aee9c131d2422d4ad
Sangwon Lim <angwonlim@gmail.com>
2021-05-19 16:19:19 -0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+__version__ = (0, 0, 1)
+
+setup(
+    name="sea_ice_rs",
+    description="Sea Ice Remote Sensing",
+    version=".".join(str(d) for d in __version__),
+    author="Sangwon Lim",
+    author_email="sangwonl@uvic.ca",
+    packages=["sea_ice_rs"],
+    include_package_data=True,
+    scripts="""
+    """.split(),
+)
