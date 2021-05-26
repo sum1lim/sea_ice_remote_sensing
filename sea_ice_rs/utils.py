f276cc91b5720fe36ea818eacd5629ffbb0fa590
Sangwon Lim <angwonlim@gmail.com>
2021-05-25 20:37:32 -0700

    add comments

--- sea_ice_rs/utils.py
+++ sea_ice_rs/utils.py
@@ -5,6 +5,9 @@ import numpy as np
 
 
 def output_to_window(name, image):
+    """
+    output image to an interactive window
+    """
     print(f"Image: {name}")
 
     while True:
@@ -22,15 +25,22 @@ def output_to_window(name, image):
 
 
 def decompose_filepath(filepath):
+    """
+    decompose filepath into three components:
+    directory path, file name and extension
+    """
     parent_directories = filepath.split("/")[:-1]
-    indir_path = "/".join(parent_directories)
+    dir_path = "/".join(parent_directories)
     File = filepath.split("/")[-1]
     [filename, extension] = File.split(".")
 
-    return (indir_path, filename, extension)
+    return (dir_path, filename, extension)
 
 
 def mkdir_output(inFile_path, appending_tail_string, extension, outImage):
+    """
+    make a new diretory and store the image in it
+    """
     (inDir, filename, _) = decompose_filepath(inFile_path)
 
     outDir = f"{inDir}_{appending_tail_string}"
