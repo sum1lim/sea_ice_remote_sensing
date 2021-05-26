bae30a472106778c6c9bf93f68e9649ddd68a85f
Sangwon Lim <angwonlim@gmail.com>
2021-05-25 20:31:42 -0700

    remove redundant lines in output_to_window

--- sea_ice_rs/utils.py
+++ sea_ice_rs/utils.py
@@ -4,45 +4,21 @@ import os
 import numpy as np
 
 
-def output_to_window(name, image, boundaries=None):
+def output_to_window(name, image):
     print(f"Image: {name}")
-    image = np.array(image)
-
-    print(f"Height: {str(len(image))}")
-    print(f"Width: {str(len(image[0]))}\n")
-
-    original_height = len(image)
-    original_width = len(image[0])
-
-    scale = 5000 / original_height
-
-    new_height = int(original_height * scale)
-    new_width = int(original_width * scale)
-    dsize = (new_width, new_height)
-    try:
-        window_output = cv2.resize(image, dsize)
-    except cv2.error:
-        window_output = cv2.resize(image.astype(np.uint8), dsize)
-
-    left = 0
-    right = original_width - 1
-    top = 0
-    bottom = original_height - 1
-
-    output = image.copy()
 
     while True:
         window_name = name + "(press q to quit)"
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
         cv2.imshow(
             window_name,
-            window_output,
+            image,
         )
 
         keyboard_input = cv2.waitKey(0)
 
         if keyboard_input == ord("q"):
-            return output
+            return
 
 
 def decompose_filepath(filepath):
