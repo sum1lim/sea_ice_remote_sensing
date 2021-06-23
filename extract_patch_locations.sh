8e9d204885330e860e0543b9aff94c64f2607225
Sangwon Lim <angwonlim@gmail.com>
2021-06-22 23:00:22 -0700

    update extract_patch_locations.sh

--- extract_patch_locations.sh
+++ extract_patch_locations.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl https://raw.githubusercontent.com/asylve/Sea-Ice/main/Images/Region-Grid.png --output ./data/AOIs.png
 extract-colour --input ./data/AOIs.png --colour R --extension png            
 threshold --input ./data/AOIs_R.png --max 255 --min 104 --extension png
 connect-lines --input ./data/AOIs_R_thresh.png --iterations 2 --extension png
