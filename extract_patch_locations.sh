50ddaf12c3024a76d606f7c0124e63f7255c267e
Sangwon Lim <angwonlim@gmail.com>
2021-06-22 22:57:26 -0700

    bash script for extract_patch_locations

---
+++ extract_patch_locations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+extract-colour --input ./data/AOIs.png --colour R --extension png            
+threshold --input ./data/AOIs_R.png --max 255 --min 104 --extension png
+connect-lines --input ./data/AOIs_R_thresh.png --iterations 2 --extension png
+centroids --input ./data/AOIs_R_thresh_CL.png --max-area 700
